7e2691c0c835795a73b23cf47317015446abeff9
zlemihl <terence.a.oneill@gmail.com>
2021-07-19 18:49:35 -0500

    jwt

--- rest-taco/src/main/java/los/campesinos/resttaco/config/WebSecurityConfig.java
+++ rest-taco/src/main/java/los/campesinos/resttaco/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -32,8 +33,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/oauth/token").permitAll()
+                .antMatchers("/oauth/token", "/authenticate", "/register").permitAll()
                 .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .httpBasic()
                 .and()
--- rest-taco/src/main/java/los/campesinos/resttaco/web/LoginController.java
+++ rest-taco/src/main/java/los/campesinos/resttaco/web/LoginController.java
@@ -1,9 +1,16 @@
 package los.campesinos.resttaco.web;
 
 import los.campesinos.resttaco.data.UserRepository;
-import los.campesinos.resttaco.domain.RegistrationForm;
-import los.campesinos.resttaco.domain.User;
+import los.campesinos.resttaco.domain.AuthenticationRequest;
+import los.campesinos.resttaco.domain.AuthenticationResponse;
+import los.campesinos.resttaco.services.CustomUserDetailsService;
+import los.campesinos.resttaco.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -13,20 +20,38 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins="*")
 public class LoginController {
 
-    UserRepository userRepo;
-    PasswordEncoder pw = new BCryptPasswordEncoder();
+    private UserRepository userRepo;
+    private PasswordEncoder pw = new BCryptPasswordEncoder();
+    private AuthenticationManager authMan;
+    private CustomUserDetailsService userDetailsService;
+    private JwtUtil jwtTokenUtil;
 
     @Autowired
-    public LoginController(UserRepository userRepo, PasswordEncoder pw) {
+    public LoginController(UserRepository userRepo, PasswordEncoder pw, AuthenticationManager aM,
+                           CustomUserDetailsService cuds, JwtUtil jwt) {
         this.userRepo = userRepo;
         this.pw = pw;
+        this.authMan = aM;
+        this.userDetailsService = cuds;
+        this.jwtTokenUtil = jwt;
     }
 
-//    @PostMapping(consumes="application/json")
-//    public User login(@RequestBody RegistrationForm login){
-//
-//
-//    }
+    @PostMapping(consumes="application/json")
+    public ResponseEntity<?> login(@RequestBody AuthenticationRequest login) throws Exception{
+        try{
+            authMan.authenticate(
+                    new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword())
+            );
+        }
+        catch (BadCredentialsException incredible) {
+            throw new Exception("Incorrect username or password...\nYou're incredible!", incredible);
+        }
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(login.getUsername());
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
 
 
 }
