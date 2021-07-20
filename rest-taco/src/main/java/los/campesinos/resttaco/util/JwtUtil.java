bb5161786c6108c30adebbb71e56d0a8cbf8d94b
zlemihl <terence.a.oneill@gmail.com>
2021-07-19 22:15:04 -0500

    environment var

--- rest-taco/src/main/java/los/campesinos/resttaco/util/JwtUtil.java
+++ rest-taco/src/main/java/los/campesinos/resttaco/util/JwtUtil.java
@@ -3,6 +3,7 @@ package los.campesinos.resttaco.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +14,8 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtil {
-    //TODO: make SECRET_KEY an environment variable, once working...
-    private String SECRET_KEY = "secret";
+    @Value("${secret}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token){
         return extractClaim(token, Claims::getSubject);
